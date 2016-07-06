fd6da9c062aeeeb70eb24465e1798bd439d3161f
Aveletsky <ixus.van.axel@gmail.com>
2016-07-07 02:15:54 +0300

    done freq massive maker and sorter by keys

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 "use strict";
 
 /**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
+ * comments
+ * @param {Array} data
+ * @return {object} obj
  */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
+module.exports = function (data) {
+
+    var freqMassive = {};
+    var sortMassive = [];
+
+    //makes freq hash
+    for (var i = 0; i < data.length; i++) {
+        ( typeof freqMassive[data[i]] === 'undefined' ) ? freqMassive[data[i]] = 1 : freqMassive[data[i]]++;
+    }
+
+    sortMassive = Object.keys(freqMassive)
+        .sort(function(a,b){
+            return freqMassive[a]-freqMassive[b]
+        });
+
+    return [freqMassive, sortMassive];
 };
--- test/test.js
+++ test/test.js
@@ -1,47 +1,28 @@
 "use strict";
 
 var expect = require('chai').expect;
-var numFormatter = require('../index');
+var abfuscator = require('../index');
 
-describe('#numFormatter', function() {
-    it('should convert single digits', function() {
-        var result = numFormatter(1);
-        expect(result).to.equal('1');
+describe('#Obfuscator', function() {
+    it('should return frequency massive ', function() {
+        var data = ['asqq','qwww','asqq','vvf','vvf','vvf','gg'],
+        expectJson={
+            'asqq':2,
+            'qwww':1,
+            'vvf':3,
+            'gg':1
+        };
+
+        var result = abfuscator(data)[0];
+        expect(result).to.deep.equal(expectJson);
     });
 
-    it('should convert double digits', function() {
-        var result = numFormatter(12);
-        expect(result).to.equal('12');
+    it('should return sorted frequency massive ', function() {
+        var data = ['asqq','qwww','asqq','vvf','vvf','vvf','gg'],
+            expectJson=[
+                'qwww','gg','asqq','vvf'
+            ];
+        var result = abfuscator(data)[1];
+        expect(result).to.deep.equal(expectJson);
     });
-
-    it('should convert triple digits', function() {
-        var result = numFormatter(123);
-        expect(result).to.equal('123');
-    });
-
-    it('should convert 4 digits', function() {
-        var result = numFormatter(1234);
-        expect(result).to.equal('1,234');
-    });
-
-    it('should convert 5 digits', function() {
-        var result = numFormatter(12345);
-        expect(result).to.equal('12,345');
-    });
-
-    it('should convert 6 digits', function() {
-        var result = numFormatter(123456);
-        expect(result).to.equal('123,456');
-    });
-
-    it('should convert 7 digits', function() {
-        var result = numFormatter(1234567);
-        expect(result).to.equal('1,234,567');
-    });
-
-    it('should convert 8 digits', function() {
-        var result = numFormatter(12345678);
-        expect(result).to.equal('12,345,678');
-    });
-
 });
