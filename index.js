fa411e60f4bbe865afb84e490525de16c9a7ef44
Aveletsky <ixus.van.axel@gmail.com>
2016-07-10 02:01:36 +0300

    cleaning code split to two separated modules add some JSDoc information

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 "use strict";
 
 /**
- * comments
+ * Obfuscator module.
+ * @module obfuscator
+ * @see module:obfuscator
  * @param {Array} data
+ * @return {Array} frequencyArray
+ * @return {Array} reversedSortedArray
  * @return {object} resultObject
  */
 module.exports = function (data) {
 
-  var frequencyMassive = {};     // 'origin key':frequency
-  var resultObject = {};
-
+  var PermutationsWithRepetition = require('./lib/PermutationsWithRepetition');
+  var frequencyArray = {};     // 'origin key':frequency
 
   //makes frequency hash
   data.forEach(function (item) {
-    ( typeof frequencyMassive[item] === 'undefined' )
-      ? frequencyMassive[item] = 1
-      : frequencyMassive[item]++;
+    ( typeof frequencyArray[item] === 'undefined' )
+      ? frequencyArray[item] = 1
+      : frequencyArray[item]++;
   });
 
-  //sorting and reversing frequencyMassive by frequency
-  var reversedSortedMassive =         // index:'origin key'
+  //sorting and reversing frequencyArray by frequency
+  var reversedSortedArray =         // index:'origin key'
     Object
-      .keys(frequencyMassive)
+      .keys(frequencyArray)
       .sort(function (a, b) {
-        return frequencyMassive[a] - frequencyMassive[b]
+        return frequencyArray[a] - frequencyArray[b]
       })
       .reverse();
 
@@ -41,82 +44,38 @@ module.exports = function (data) {
   }
   //endregion
 
-  //region PermutationsWithRepetition
-  function PermutationsWithRepetition(src, len){
 
-    var K = len - 1, k = 0,
-      N = src.length, n = 0,
-      out = [],
-      stack = [];
+  var resultObject = {}; // 'original key':'alias key'
 
-    function next(){
-      while (true) {
-        while (n < src.length) {
-          out[k] = src[n++];
-          if (k == K) return out.slice(0);
-          else {
-            if (n < src.length) {
-              stack.push(k);
-              stack.push(n);
-            }
-            k++;
-            n = 0;
-          }
-        }
-        if (stack.length == 0) break;
-
-        n = stack.pop();
-        k = stack.pop();
-      }
-      return false;
-    }
-
-    function rewind(){ k = 0; n = 0; out = []; stack = []; }
-
-    function each(cb) {
-      rewind();
-      var v;
-      while (v = next()) if (cb(v) === false) return;
-    }
-
-    return {
-      next: next,
-      each: each,
-      rewind: rewind
-    };
-  }
-  //endregion
-
-  var resultObject = {}; // 'original key':reserved length
-
-  reversedSortedMassive.forEach(
-    function(item,iter){
-      resultObject[item]=dictionaryShort[iter % dictionaryShort.length];
+  //fill all object with first character from short dictionary
+  reversedSortedArray.forEach(
+    function (item, iter) {
+      resultObject[item] = dictionaryShort[iter % dictionaryShort.length];
     }
   );
 
-  var avablePosions=1;
-  var perms = PermutationsWithRepetition(dictionaryFull, avablePosions);
-  reversedSortedMassive.forEach(
-    function(item,iter){
-      var curGeneratedCombination = perms.next();
-      if(!curGeneratedCombination){
-        avablePosions++;
-        perms = PermutationsWithRepetition(dictionaryFull, avablePosions);
-        curGeneratedCombination = perms.next();
-      }
+  var availablePosWindow = 1;
 
-      if(iter>dictionaryShort.length){
-        resultObject[item]+=curGeneratedCombination.join('');
-      }
+  //fill others positions with character from full dictionary
+  var permutations = PermutationsWithRepetition(dictionaryFull, availablePosWindow);
+  reversedSortedArray.forEach(
+    function (item, iter) {
+      if (iter >= dictionaryShort.length) {
+        var curGeneratedCombination = permutations.next();
+        if (!curGeneratedCombination) {
+          availablePosWindow++;
+          permutations = PermutationsWithRepetition(dictionaryFull, availablePosWindow);
+          curGeneratedCombination = permutations.next();
+        }
 
+        resultObject[item] += curGeneratedCombination.join('');
+      }
     }
-
   );
 
   return {
-    freqMassive:frequencyMassive,
-    revSortMassive:reversedSortedMassive,
-    obfuscatedObject:resultObject
+    freqArray: frequencyArray,
+    revSortArray: reversedSortedArray,
+    obfuscatedObject: resultObject
   }
 };
--- test/test.js
+++ test/test.js
@@ -5,8 +5,8 @@ var abfuscator = require('../index');
 
 
 describe('#Obfuscator', function () {
-  ///frequencyMassive, reversalSortedMassive, reservedLengthForAlias, resultObject
-  it('should return frequency massive ', function () {
+  ///freqArray revSortArray obfuscatedObject
+  it('should return frequency Array ', function () {
     var data = ['asqq', 'qwww', 'asqq', 'vvf', 'vvf', 'vvf', 'gg'],
       expectJson = {
         'asqq': 2,
@@ -15,35 +15,28 @@ describe('#Obfuscator', function () {
         'gg': 1
       };
 
-    var result = abfuscator(data)[0];
+    var result = abfuscator(data).freqArray;
     expect(result).to.deep.equal(expectJson);
   });
 
-  it('should return sorted&reversed frequency massive without number values', function () {
+  it('should return sorted&reversed frequency Array without number values', function () {
     var data = ['asqq', 'qwww', 'asqq', 'vvf', 'vvf', 'vvf', 'gg'],
       expectJson = ['vvf', 'asqq', 'gg', 'qwww'];
-    var result = abfuscator(data)[1];
-    expect(result).to.deep.equal(expectJson);
-  });
-
-  it('should return alias map with amount reserved length of new alie', function () {
-    var data = ['asqq', 'qwww', 'asqq', 'vvf', 'vvf', 'vvf', 'gg'],
-      expectJson = {vvf: 1, asqq: 1, gg: 1, qwww: 1};
-    var result = abfuscator(data)[2];
+    var result = abfuscator(data).revSortArray;
     expect(result).to.deep.equal(expectJson);
   });
 
   it('should return result of obfuscate', function () {
     var data = ['asqq', 'qwww', 'asqq', 'vvf', 'vvf', 'vvf', 'gg'],
       expectJson = {vvf: 'a', asqq: 'b', gg: 'c', qwww: 'd'};
-    var result = abfuscator(data)[3];
+    var result = abfuscator(data).obfuscatedObject;
     expect(result).to.deep.equal(expectJson);
   });
 
-  it('check compatable of module', function () {
+  it('check the efficiency of module', function () {
     var data = require('./data.json');
     var expectJson = require('./resultOfObfuscation.json');
-    var result = abfuscator(data)[3];
+    var result = abfuscator(data).obfuscatedObject;
     expect(result).to.deep.equal(expectJson);
   });
 
